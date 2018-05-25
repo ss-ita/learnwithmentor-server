dd81ea9bba1b7fbc996ca9cbd3c4423dc55dcff5
romanparobiy <romik1009@gmail.com>
2018-05-25 18:22:27 +0300

    New database structure

--- DB/SQLCreateDataBaseLearnWithMentor.sql
+++ DB/SQLCreateDataBaseLearnWithMentor.sql
@@ -6,17 +6,6 @@ CREATE DATABASE LearnWithMentor_DB
 Go 
 use LearnWithMentor_DB
 
-CREATE TABLE Users
-(
-    Id INT IDENTITY,
-    Name NVARCHAR(50) NOT NULL,
-    Email NVARCHAR(50) NOT NULL,
-    Password NVARCHAR(50) NOT NULL,
-
-CONSTRAINT PK_User_Id PRIMARY KEY (Id),
-CONSTRAINT UQ_User_Email UNIQUE (Email)
-)
-
 CREATE TABLE Roles
 (
     Id INT IDENTITY,
@@ -25,13 +14,17 @@ CREATE TABLE Roles
 CONSTRAINT PK_Role_Id PRIMARY KEY (Id),
 )
 
-CREATE TABLE UserRoles
+CREATE TABLE Users
 (
-    Role_Id INT NOT NULL,
-    User_Id INT NOT NULL,    
-    
- CONSTRAINT FK_UserRoles_To_Roles FOREIGN KEY (Role_Id)  REFERENCES Roles (Id),
- CONSTRAINT FK_UserRoles_To_Users FOREIGN KEY (User_Id)  REFERENCES Users (Id)
+    Id INT IDENTITY,
+    Name NVARCHAR(50) NOT NULL,
+    Email NVARCHAR(50) NOT NULL,
+    Password NVARCHAR(50) NOT NULL,
+	Role_Id INT NOT NULL,
+
+CONSTRAINT PK_Users_Id PRIMARY KEY (Id),
+CONSTRAINT UQ_Users_Email UNIQUE (Email),
+CONSTRAINT FK_Users_To_Roles FOREIGN KEY (Role_Id)  REFERENCES Roles (Id)
 )
 
 CREATE TABLE Groups
@@ -61,10 +54,23 @@ CREATE TABLE Plans
     Name NVARCHAR(50) NOT NULL,
     Description NVARCHAR(500),
     Published BIT  NOT NULL CONSTRAINT DF_Plans_Published  DEFAULT  0,
-    Owner_Id INT NOT NULL,
+    Create_Id INT NOT NULL,
+	Mod_Id INT,
+	Create_Date DATETIME NOT NULL,
+	Mod_Date DATETIME,
+
 
 CONSTRAINT PK_Plans_Id PRIMARY KEY (Id),
-CONSTRAINT FK_Plans_To_Users FOREIGN KEY (Owner_Id)  REFERENCES Users (Id)
+CONSTRAINT FK_Plans_To_UsersC FOREIGN KEY (Create_Id)  REFERENCES Users (Id),
+CONSTRAINT FK_Plans_To_UsersM FOREIGN KEY (Mod_Id)  REFERENCES Users (Id)
+)
+
+CREATE TABLE Sections
+(
+	Id INT IDENTITY,
+	Name NVARCHAR(50) NOT NULL,
+
+CONSTRAINT PK_Sections_Id PRIMARY KEY (Id),
 )
 
 CREATE TABLE GroupPlans
@@ -81,43 +87,42 @@ CREATE TABLE Tasks
     Id INT IDENTITY,
     Name NVARCHAR(50) NOT NULL,
     Description NVARCHAR(MAX),
+	Private BIT NOT NULL CONSTRAINT DF_Tasks_Private DEFAULT 1,
+	Create_Id INT NOT NULL,
+	Mod_Id INT,
+	Create_Date DATETIME NOT NULL,
+	Mod_Date DATETIME,
     
 CONSTRAINT PK_Tasks_Id PRIMARY KEY (Id),
+CONSTRAINT FK_Tasks_To_UsersC FOREIGN KEY (Create_Id)  REFERENCES Users (Id),
+CONSTRAINT FK_Tasks_To_UsersM FOREIGN KEY (Mod_Id)  REFERENCES Users (Id)
 )
 
 CREATE TABLE PlanTasks
 (
     Plan_Id INT NOT NULL,
-    Task_Id INT NOT NULL,    
+    Task_Id INT NOT NULL,
+	Priority INT,
+	Section_Id INT,   
     
  CONSTRAINT FK_PlanTasks_To_Plans FOREIGN KEY (Plan_Id)  REFERENCES Plans (Id),
- CONSTRAINT FK_PlanTasks_To_Tasks FOREIGN KEY (Task_Id)  REFERENCES Tasks (Id)
-)
-
-CREATE TABLE Activities
-(
-    Id INT IDENTITY,
-    Group_Id INT NOT NULL,
-    Task_Id INT NOT NULL,
-    End_Date DATETIME,
-
-CONSTRAINT PK_Activities_Id PRIMARY KEY (Id),
-CONSTRAINT FK_Activities_To_Groups FOREIGN KEY (Group_Id)  REFERENCES Groups (Id),
-CONSTRAINT FK_Activities_To_Tasks FOREIGN KEY (Task_Id)  REFERENCES Tasks (Id)
+ CONSTRAINT FK_PlanTasks_To_Tasks FOREIGN KEY (Task_Id)  REFERENCES Tasks (Id),
+ CONSTRAINT FK_PlanTasks_To_Sections FOREIGN KEY (Section_Id)  REFERENCES Sections (Id)
 )
 
 CREATE TABLE UserTasks
 (
     Id INT IDENTITY,
     User_Id INT NOT NULL,
-    Activity_Id INT NOT NULL,    
+    Task_Id INT NOT NULL,    
     State NCHAR NOT NULL CONSTRAINT DF_UserTasks_State DEFAULT 'P',
-    End_Date DATETIME,
-    Result NVARCHAR(MAX) NOT NULL,    
+    End_Date DATETIME NOT NULL,
+    Result NVARCHAR(MAX) NOT NULL, 
+	Propose_End_Date DATETIME,
 
  CONSTRAINT PK_UserTasks_Id PRIMARY KEY (Id),
  CONSTRAINT FK_UserTasks_To_Users FOREIGN KEY (User_Id)  REFERENCES Users (Id),
- CONSTRAINT FK_UserTasks_To_Activities FOREIGN KEY (Activity_Id)  REFERENCES Activities (Id),
+ CONSTRAINT FK_UserTasks_To_Activities FOREIGN KEY (Task_Id)  REFERENCES Tasks (Id),
  CONSTRAINT CK_UserTasks_State CHECK(State IN ('P', 'D', 'A', 'R'))
 )
 
@@ -141,14 +146,6 @@ CREATE TABLE UserTasksMessages
  CONSTRAINT FK_UserTasksMessages_To_UserTasks FOREIGN KEY (Usertask_Id)  REFERENCES UserTasks (Id)
 )
 
-CREATE TABLE EndDateSuggestions
-(
-    Usertask_Id INT NOT NULL,
-    End_Date DATETIME NOT NULL,
-    
- CONSTRAINT FK_EndDateSuggestions_To_UserTasks FOREIGN KEY (Usertask_Id)  REFERENCES UserTasks (Id)
-)
-
 CREATE TABLE PlanSuggestion
 (
     Plan_Id INT NOT NULL,
@@ -161,16 +158,19 @@ CREATE TABLE PlanSuggestion
  CONSTRAINT FK_PlanSuggestion_To_Users1 FOREIGN KEY (Mentor_Id)  REFERENCES Users (Id)
 )
 
-CREATE TABLE Comment
+CREATE TABLE Comments
 (
     Id INT IDENTITY,
-    User_Id INT,
     Task_Id INT NOT NULL,    
     Text NVARCHAR(2000) NOT NULL,
-    Publish_Date DATETIME NOT NULL,
-    Edit_Date DATETIME ,
+	Create_Id INT NOT NULL,
+	Mod_Id INT,
+	Create_Date DATETIME NOT NULL,
+	Mod_Date DATETIME,
 
- CONSTRAINT PK_Comment_Id PRIMARY KEY (Id),
- CONSTRAINT FK_Comment_To_Users FOREIGN KEY (User_Id)  REFERENCES Users (Id),
- CONSTRAINT FK_Comment_To_Tasks FOREIGN KEY (Task_Id)  REFERENCES Tasks (Id)
+ CONSTRAINT PK_Comments_Id PRIMARY KEY (Id),
+ CONSTRAINT FK_Comments_To_Tasks FOREIGN KEY (Task_Id)  REFERENCES Tasks (Id),
+ CONSTRAINT FK_Comments_To_UsersC FOREIGN KEY (Create_Id)  REFERENCES Users (Id),
+ CONSTRAINT FK_Comments_To_UsersM FOREIGN KEY (Mod_Id)  REFERENCES Users (Id)
 )
+
