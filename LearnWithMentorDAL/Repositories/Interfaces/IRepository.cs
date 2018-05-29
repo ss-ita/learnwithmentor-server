a119747e5fd988ea1d442696b9fb722e5ab510e5
romanparobiy <romik1009@gmail.com>
2018-05-29 18:48:35 +0300

    Added base repository

---
+++ LearnWithMentorDAL/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWithMentorDAL.Repositories
+{
+    interface IRepository<T> where T: class
+    {
+        IEnumerable<T> GetAll();
+        void Add(T item);
+        void Update(T item);
+        void Remove(T item);
+    }
+}
