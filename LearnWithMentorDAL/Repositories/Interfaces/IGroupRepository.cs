b0c8c5caf4f443d9577980fe63726361c31d124f
romanparobiy <romik1009@gmail.com>
2018-05-30 11:57:40 +0300

    Added GroupRepository

---
+++ LearnWithMentorDAL/Repositories/Interfaces/IGroupRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnWithMentorDAL.Entities;
+
+namespace LearnWithMentorDAL.Repositories
+{
+    interface IGroupRepository: IRepository<Group>
+    {
+        Group Get(int id);
+    }
+}
