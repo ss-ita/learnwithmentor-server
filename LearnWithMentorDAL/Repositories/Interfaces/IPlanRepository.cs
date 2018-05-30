ba709fe5c9f5800fae524357c8feccc88027c6c4
romanparobiy <romik1009@gmail.com>
2018-05-30 12:11:45 +0300

    Added PlanRepository

---
+++ LearnWithMentorDAL/Repositories/Interfaces/IPlanRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnWithMentorDAL.Entities;
+
+namespace LearnWithMentorDAL.Repositories
+{
+    interface IPlanRepository: IRepository<Plan>
+    {
+        Plan Get(int id);
+    }
+}
---
+++ LearnWithMentorDAL/Repositories/PlanRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnWithMentorDAL.Entities;
+
+namespace LearnWithMentorDAL.Repositories
+{
+    class PlanRepository: BaseRepository<Plan>, IPlanRepository
+    {
+        public PlanRepository(LearnWithMentor_DBEntities _context) : base(_context)
+        {
+        }
+        public Plan Get(int id)
+        {
+            return context.Plans.Where(p => p.Id == id).FirstOrDefault();
+        }
+    }
+}
