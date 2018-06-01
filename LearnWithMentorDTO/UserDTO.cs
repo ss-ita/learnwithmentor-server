0b242541624f2a3d59f97e70f6f2e13e59a0aa8e
romanparobiy <romik1009@gmail.com>
2018-06-01 17:08:36 +0300

    Added list users endpoint

---
+++ LearnWithMentorDTO/UserDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWithMentorDTO
+{
+    public class UserDTO
+    {
+        public UserDTO(int _id, string _firstName, string _lastName, string _email, string _role)
+        {
+            LastName = _lastName;
+            FirstName = _firstName;
+            Id = _id;
+            Email = _email;
+            Role = _role;
+        }
+        public string LastName { set; get; }
+        public string FirstName { set; get; }
+        public int Id { set; get; }
+        public string Email { set; get; }
+        public string Role { set; get; }
+    }
+}
