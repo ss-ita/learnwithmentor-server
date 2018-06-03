01d4bbe1f82fbfc9f3e0977972606a0c0bf424c5
Roman <maksymyshyn.roman@gmail.com>
2018-06-03 15:53:48 +0300

    Created TaskDTO, api TaskController

--- ContextsTest/Program.cs
+++ ContextsTest/Program.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LearnWithMentorDAL;
 using LearnWithMentorDAL.Entities;
+using LearnWithMentorDTO;
 
 namespace ContextsTest
 {
@@ -18,7 +20,21 @@ namespace ContextsTest
                 foreach (var g in db.Users)
                     Console.WriteLine("User: {0} {1}", g.FirstName, g.LastName);
             }
-
+            UnitOfWork UoW = new UnitOfWork(new LearnWithMentor_DBEntities());
+            List<TaskDTO> dto = new List<TaskDTO>();
+            foreach (var t in UoW.Tasks.GetAll())
+            {
+                dto.Add(new TaskDTO(t.Id,
+                                    t.Name,
+                                    t.Description,
+                                    t.Private,
+                                    t.Create_Id,
+                                    t.Mod_Id,
+                                    t.Create_Date,
+                                    t.Mod_Date,
+                                    null,
+                                    null));
+            }
             Console.ReadKey();
         }
     }
---
+++ LearnWithMentorDTO/TaskDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWithMentorDTO
+{
+    public class TaskDTO
+    {
+        public TaskDTO(int _id,
+                        string _name,
+                        string _description,
+                        bool _private,
+                        int _create_id,
+                        Nullable<int> _mod_id,
+                        Nullable<System.DateTime> _create_date,
+                        Nullable<System.DateTime> _mod_date,
+                        Nullable<int> _priority,
+                        Nullable<int> _section_id)
+        {
+            Id = _id;
+            Name = _name;
+            Description = _description;
+            Private = _private;
+            Create_Date = _create_date;
+            Mod_Date = _mod_date;
+            Creator_Id = _create_id;
+            Modifier_Id = _mod_id;
+            Priority = _priority;
+            Section_Id = _section_id;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Private { get; set; }
+        public int Creator_Id { get; set; }
+        public Nullable<int> Modifier_Id { get; set; }
+        public Nullable<System.DateTime> Create_Date { get; set; }
+        public Nullable<System.DateTime> Mod_Date { get; set; }
+        public Nullable<int> Priority { get; set; }
+        public Nullable<int> Section_Id { get; set; }
+    }
+}
