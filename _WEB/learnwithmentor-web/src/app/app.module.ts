9ce82044d12cf27489b84f8fc2e02472d1c95980
Yulia Pavlyk <pavlyk.yuliia@gmail.com>
2018-06-03 16:50:21 +0300

    create user component

--- _WEB/learnwithmentor-web/src/app/app.module.ts
+++ _WEB/learnwithmentor-web/src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { UsersPageComponent } from './users-page/users-page.component';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import { UserBlockComponent } from './user-block/user-block.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersPageComponent,
+    UserBlockComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatIconModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
