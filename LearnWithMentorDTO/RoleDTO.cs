eaa102739b20428350d3ba9db86ac6626addca1e
romanparobiy <romik1009@gmail.com>
2018-06-05 11:55:38 +0300

    Updated UserController funtional

---
+++ LearnWithMentorDTO/RoleDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnWithMentorDTO
+{
+    public class RoleDTO
+    {
+        public RoleDTO(int _id, string _name)
+        {
+            Id = _id;
+            Name = _name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
