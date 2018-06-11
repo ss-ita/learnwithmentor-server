1a521bde63abbc75f09d2bacd787729e7b2f7249
romanparobiy <romik1009@gmail.com>
2018-06-11 21:32:02 +0300

    replaced entities

--- LearnWithMentorDAL/Entities/PlanTask.cs
+++ LearnWithMentorDAL/Entities/PlanTask.cs
@@ -14,11 +14,20 @@ namespace LearnWithMentorDAL.Entities
     
     public partial class PlanTask
     {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public PlanTask()
+        {
+            this.Comments = new HashSet<Comment>();
+        }
+    
+        public int Id { get; set; }
         public int Plan_Id { get; set; }
         public int Task_Id { get; set; }
         public Nullable<int> Priority { get; set; }
         public Nullable<int> Section_Id { get; set; }
     
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Comment> Comments { get; set; }
         public virtual Plan Plans { get; set; }
         public virtual Section Sections { get; set; }
         public virtual Task Tasks { get; set; }
