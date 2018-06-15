36c0631ded5754dc8d111c7b2344221726b27cca
Roman Maksymyshyn <rmaksytc@training.local>
2018-06-15 16:45:10 +0300

    Added baseservice

---
+++ LearnWithMentorBLL/Services/BaseService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LearnWithMentorBLL.Interfaces;
+using LearnWithMentorDTO;
+using LearnWithMentorDAL.UnitOfWork;
+using LearnWithMentorDAL.Entities;
+
+namespace LearnWithMentorBLL.Services
+{
+    class BaseService
+    {
+        protected readonly IUnitOfWork db;
+
+        public BaseService()
+        {
+            db = new UnitOfWork(new LearnWithMentor_DBEntities());
+        }
+    }
+}
--- LearnWithMentorBLL/Services/TaskService.cs
+++ LearnWithMentorBLL/Services/TaskService.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LearnWithMentorBLL.Interfaces;
+using LearnWithMentorDTO;
 
 namespace LearnWithMentorBLL.Services
 {
-    class TaskService
+    class TaskService : BaseService,ITaskService
     {
+        public TaskService() : base()
+        {
+        }
+        public void DeleteTaskById(int id)
+        {
+            
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TaskDTO GetTasks(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveTaskById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TaskDTO> Search(string[] str, int planId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TaskDTO> Search(string[] str)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
