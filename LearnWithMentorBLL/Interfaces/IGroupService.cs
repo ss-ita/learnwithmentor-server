a8a212ca64181b667b9b4255963a01ca00797a4e
yuliiapavlyk <pavlyk.yuliia@gmail.com>
2018-06-19 13:46:58 +0300

    add group service

---
+++ LearnWithMentorBLL/Interfaces/IGroupService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using LearnWithMentorDAL;
+using LearnWithMentorDAL.Entities;
+using LearnWithMentorDTO;
+
+namespace LearnWithMentorBLL.Interfaces
+{
+    public interface IGroupService
+    {
+        GroupDTO GetGroupById(int id);
+        IEnumerable<GroupDTO> GetGroupsByMentor(int mentorId);
+        IEnumerable<UserDTO> GetUsers(int groupId);
+        bool AddGroup(GroupDTO group);
+        bool AddUserToGroup(int userId, int groupId);
+        bool AddPlanToGroup(int planId, int groupId);
+    }
+}
