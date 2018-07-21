56659d875af74f7558817fa7a4e651b37861d2f6
romanparobiy <romik1009@gmail.com>
2018-07-21 18:30:14 +0300

    Created NUnit test project with test discovering in test explorer

---
+++ LearnWithMentor.Tests/Controllers.Tests/UserController.Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Moq;
+
+namespace LearnWithMentor.Tests.Controllers.Tests
+{
+    [TestFixture]
+    public class UserControllerTests
+    {
+        [Test]
+        public void ShouldReturnAllUsers()
+        {
+            Assert.True(true);
+        }
+    }
+}
