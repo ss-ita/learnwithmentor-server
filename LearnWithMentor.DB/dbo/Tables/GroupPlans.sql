cb833f263ebee163067551bab1bee34ebbee0ffc
Yura Stashko <yura.stashko98@gmail.com>
2018-09-06 16:02:14 +0300

    Created DB Server project (#247)
    * Closes #240 Created DB Server project
    
    * Closes #240 Fixed bugs in SQL Server DB Project
    
    * Closes #240 Return Comments.sql which was deleted by myself
    
    * Added empty lines

---
+++ LearnWithMentor.DB/dbo/Tables/GroupPlans.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE GroupPlans
+(
+    Group_Id INT NOT NULL,
+    Plan_Id INT NOT NULL,    
+    
+ CONSTRAINT FK_GroupPlans_To_Groups FOREIGN KEY (Group_Id)  REFERENCES Groups (Id),
+ CONSTRAINT FK_GroupPlans_To_Plans FOREIGN KEY (Plan_Id)  REFERENCES Plans (Id)
+)
