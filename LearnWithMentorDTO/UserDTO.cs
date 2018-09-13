7127b82ed39fb1d42760e1fa9aeb59357d9a7c17
supermaan19 <yurikozlovskij@gmail.com>
2018-09-13 18:13:14 +0300

    Some changes

--- LearnWithMentorDTO/UserDTO.cs
+++ LearnWithMentorDTO/UserDTO.cs
@@ -5,11 +5,12 @@ namespace LearnWithMentorDTO
 {
     public class UserDTO
     {
-        public UserDTO(int id, string firstName, string lastName, string role, bool blocked, bool emailConfirmed)
+        public UserDTO(int id, string firstName, string lastName, string email,string role, bool blocked, bool emailConfirmed)
         {
             LastName = lastName;
             FirstName = firstName;
             Id = id;
+            Email = email;
             Role = role;
             Blocked = blocked;
             EmailConfirmed = emailConfirmed;
@@ -29,6 +30,7 @@ namespace LearnWithMentorDTO
             ErrorMessage = "FirstName not valid")]
         public string FirstName { set; get; }
         public int Id { set; get; }
+        public string Email { set; get; }
         public string Role { set; get; }
         public bool? Blocked { set; get; }
         public bool EmailConfirmed { set; get; }
