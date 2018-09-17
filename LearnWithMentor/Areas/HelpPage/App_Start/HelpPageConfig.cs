0ae426824dd5a41c0a61fd03b5fe020113885a5f
Sofia <flyssoffia@gmail.com>
2018-09-17 10:12:55 +0300

    Deleted orphaned comments and $ from file

--- LearnWithMentor/Areas/HelpPage/App_Start/HelpPageConfig.cs
+++ LearnWithMentor/Areas/HelpPage/App_Start/HelpPageConfig.cs
@@ -33,15 +33,6 @@ namespace LearnWithMentor.Areas.HelpPage
             Justification = "Part of a URI.")]
         public static void Register(HttpConfiguration config)
         {
-            //// Uncomment the following to use the documentation from XML documentation file.
-           
-            //// Uncomment the following to use "sample string" as the sample for all actions that have string as the body parameter or return type.
-            //// Also, the string arrays will be used for IEnumerable<string>. The sample objects will be serialized into different media type 
-            //// formats by the available formatters.
-          
-            // Extend the following to provide factories for types not handled automatically (those lacking parameterless
-            // constructors) or for which you prefer to use non-default property values. Line below provides a fallback
-            // since automatic handling will fail and GeneratePageResult handles only a single type.
 #if Handle_PageResultOfT
             config.GetHelpPageSampleGenerator().SampleObjectFactories.Add(GeneratePageResult);
 #endif
@@ -52,21 +43,6 @@ namespace LearnWithMentor.Areas.HelpPage
             config.SetSampleForMediaType(
                 new TextSample("Binary JSON content. See http://bsonspec.org for details."),
                 new MediaTypeHeaderValue("application/bson"));
-
-            //// Uncomment the following to use "[0]=foo&[1]=bar" directly as the sample for all actions that support form URL encoded format
-            //// and have IEnumerable<string> as the body parameter or return type.
-          
-            //// Uncomment the following to use "1234" directly as the request sample for media type "text/plain" on the controller named "Values"
-            //// and action named "Put".
-          
-            //// Uncomment the following to use the image on "../images/aspNetHome.png" directly as the response sample for media type "image/png"
-            //// on the controller named "Values" and action named "Get" with parameter "id".
-          
-            //// Uncomment the following to correct the sample request when the action expects an HttpRequestMessage with ObjectContent<string>.
-            //// The sample will be generated as if the controller named "Values" and action named "Get" were having string as the body parameter.
-         
-            //// Uncomment the following to correct the sample response when the action returns an HttpResponseMessage with ObjectContent<string>.
-            //// The sample will be generated as if the controller named "Values" and action named "Post" were returning a string.
         }
 
 #if Handle_PageResultOfT
--- LearnWithMentor/GlobalSuppressions.cs
+++ LearnWithMentor/GlobalSuppressions.cs
@@ -6,3 +6,4 @@
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S1172:Remove this unused method parameter 'sampleGenerator'.", Justification = "<Ожидание>", Scope = "member", Target = "~M:LearnWithMentor.Areas.HelpPage.HelpPageSampleGenerator.DefaultSampleObjectFactory(LearnWithMentor.Areas.HelpPage.HelpPageSampleGenerator,System.Type)~System.Object")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S125:Remove this commented out code.", Justification = "<Ожидание>", Scope = "member", Target = "~M:LearnWithMentor.WebApiApplication.Application_Start")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Build", "S1075:Refactor your code not to use hardcoded absolute paths or URIs.", Justification = "<Ожидание>", Scope = "member", Target = "~M:LearnWithMentor.BundleConfig.RegisterBundles(System.Web.Optimization.BundleCollection)")]
