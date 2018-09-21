61c4ffef1c78f03aed585f0f7e0f7634e698cbca
supermaan19 <yurikozlovskij@gmail.com>
2018-09-21 22:10:11 +0300

    Closure #285 Admin can update plans and tasks

--- LearnWithMentor/Controllers/PlanController.cs
+++ LearnWithMentor/Controllers/PlanController.cs
@@ -227,7 +227,7 @@ namespace LearnWithMentor.Controllers
         /// </summary>
         /// <param name="id"> Id of plan to be updated. </param>
         /// <param name="value"> Plan info to be updated. </param>
-        [Authorize(Roles = "Mentor")]
+        [Authorize(Roles = "Mentor, Admin")]
         [HttpPut]
         [Route("api/plan/{id}")]
         public HttpResponseMessage Put(int id, [FromBody]PlanDto value)
@@ -261,7 +261,7 @@ namespace LearnWithMentor.Controllers
         /// <param name="sectionId"></param>
         /// <param name="priority"></param>
         /// <returns></returns>            
-        [Authorize(Roles = "Mentor")]
+        [Authorize(Roles = "Mentor, Admin")]
         [HttpPut]
         [Route("api/plan/{id}/task/{taskId}")]
         public HttpResponseMessage PutTaskToPlan(int id, int taskId,string sectionId, string priority)
