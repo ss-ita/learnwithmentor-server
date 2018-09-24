bf1a8257d1e395cff7a11f7a1a09740370f8df51
kravchenkov <kravchenkov.me@gmail.com>
2018-09-24 14:39:08 +0300

    Closes #297 migrated DTO to .NET standards

---
+++ LearnWithMentorDTOStandard/MessageDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using LearnWithMentorDTO.Infrastructure;
+
+namespace LearnWithMentorDTO
+{
+    public class MessageDto
+    {
+        public MessageDto(int id,
+                        int senderId,
+                        int userTaskId,
+                        string senderName,
+                        string text,
+                        DateTime? sendTime)
+        {
+            Id = id;
+            SenderId = senderId;
+            UserTaskId = userTaskId;
+            Text = text;
+            SendTime = sendTime;
+            SenderName = senderName;
+        }
+
+        public int Id { get; set; }
+        public int SenderId { get; set; }
+        public int UserTaskId { get; set; }
+        public string SenderName { get; set; }
+        [Required]
+        [StringLength(ValidationRules.MAX_MESSAGE_LENGTH,
+            ErrorMessage = "Message is too long")]
+        public string Text { get; set; }
+        public DateTime? SendTime { get; set; }
+    }
+}
